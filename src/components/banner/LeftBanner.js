8ff12b44ed86a37ccf5f70aed2d312399368b938
Atishmita <atishmitadash10@gmail.com>
2025-01-10 00:33:33 +0530

    Updated Experience component for responsive design

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -46,7 +46,7 @@ const LeftBanner=() =>{
     return (
       <div className="w-full lgl:w-1/2 flex flex-col gap-14">
         <div className="flex flex-col gap-5">
-          <h4 className="text-lg pt-3 pb-3 font-normal">WELCOME TO MY WORLD</h4>
+          <h4 className="text-lg  pt-3 pb-3 font-normal">WELCOME TO MY WORLD</h4>
           <h1 className="text-5xl font-bold text-white">
             Hi, I'm
             <span className="text-designColor capitalize"> Atishmita Dash</span>
--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -7,53 +7,53 @@ const Experience = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
-      className="w-full flex gap-20"
+      className="w-full flex flex-col lg:flex-row gap-6 lg:gap-20"
     >
-      <div>
+      <div className="w-full lg:w-1/2">
         <div className="py-12 font-titleFont flex flex-col gap-4">
           <h2 className="text-4xl font-bold">Experience</h2>
         </div>
-        <div className="flex">
-          <div className=" mt-14 w-1/2 h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10 mr-20">
-            <ResumeCard
-              title="Summer Intern"
-              subTitle="DRDO,Chandipur Balasore"
-              result="May 24 - June 24"
-              des=" Contributed in the development of GUI for signal processing applications and developed a responsive website for the Radar and Sensor Wing of DRDO using react framework."
-            />
-            <ResumeCard
-              title="Frontend Developer Intern"
-              subTitle="Lancyfirm"
-              result="Dec 22 - Mar 23"
-              des=" Contributed in the development of the frontend of the Lancyfirm official website."
-            />
-            <ResumeCard
-              title="User Interface Desiging Intern"
-              subTitle="Zopiqa"
-              result="Apr 23 - Jun 23"
-              des="During my internship at Zopiqa, I worked on creating user-friendly interfaces through research, prototyping, and collaboration with the design and development teams."
-            />
-          </div>
-          <div className=" mt-14 w-1/2 h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-            <ResumeCard
-              title="User Interface Desiging Intern"
-              subTitle="Graduate Us"
-              result="Jan 23 - Jun 23"
-              des="During my internship at Graduate Us, I worked on creating the web vesion of the official website. "
-            />
-            <ResumeCard
-              title="Graphic Desiging Intern"
-              subTitle="Wasper Tech"
-              result="Sept 22 - May 23"
-              des="During my internship at Wasper, I worked on creating Instagram Posters , templates etc. "
-            />
-            <ResumeCard
-              title="Core Member"
-              subTitle="Idea Innovation Cell , VSSUT Burla"
-              result="jun 22 - Present"
-              des="Technical team Member"
-            />
-          </div>
+        <div className="mt-14 border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Summer Intern"
+            subTitle="DRDO,Chandipur Balasore"
+            result="May 24 - June 24"
+            des="Contributed in the development of GUI for signal processing applications and developed a responsive website for the Radar and Sensor Wing of DRDO using react framework."
+          />
+          <ResumeCard
+            title="Frontend Developer Intern"
+            subTitle="Lancyfirm"
+            result="Dec 22 - Mar 23"
+            des="Contributed in the development of the frontend of the Lancyfirm official website."
+          />
+          <ResumeCard
+            title="User Interface Designing Intern"
+            subTitle="Zopiqa"
+            result="Apr 23 - Jun 23"
+            des="During my internship at Zopiqa, I worked on creating user-friendly interfaces through research, prototyping, and collaboration with the design and development teams."
+          />
+        </div>
+      </div>
+      <div className="w-full lg:w-1/2">
+        <div className="mt-14 border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="User Interface Designing Intern"
+            subTitle="Graduate Us"
+            result="Jan 23 - Jun 23"
+            des="During my internship at Graduate Us, I worked on creating the web version of the official website."
+          />
+          <ResumeCard
+            title="Graphic Designing Intern"
+            subTitle="Wasper Tech"
+            result="Sept 22 - May 23"
+            des="During my internship at Wasper, I worked on creating Instagram Posters, templates, etc."
+          />
+          <ResumeCard
+            title="Core Member"
+            subTitle="Idea Innovation Cell , VSSUT Burla"
+            result="Jun 22 - Present"
+            des="Technical team Member"
+          />
         </div>
       </div>
     </motion.div>
